d54c6e553b85af530ff54e791e995caa45253a23
Alejandro Frech <alefrech@gmail.com>
2018-01-24 18:47:30 -0600

    ...

---
+++ Tarea1/tokens.h
@@ -0,0 +1,10 @@
+#define OP_ADD 1
+#define OP_SUB 2
+#define OP_MULT 3
+#define OP_DIV 4
+#define PAR_LEFT 5
+#define PAR_RIGHT 6
+#define LIT_NUM 7
+#define TK_EOL 8
+#define TK_EOF 9
+#define TK_ERROR 999
